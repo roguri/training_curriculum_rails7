7031ee3b97bceebe30fc218cf7f5e5787e30ea9c
roguri <ryoryoma205@gmail.com>
2024-01-15 19:15:35 +0900

    Revert "Merge branch 'main' into second_training"
    This reverts commit d514a241d1c7307ce146812ed42b6470056a3895, reversing
    changes made to 59d6209a9752d7c95b8f68b0c08acf0d9cc88c41.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date,:plan)
   end
 
   def get_week
@@ -41,7 +41,6 @@ class CalendarsController < ApplicationController
       end
 
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
-
       @week_days.push(days)
     end
 
